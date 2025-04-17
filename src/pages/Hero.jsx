3b2194fc6fa4c4d5bd72346556ad2c474cd86ae1
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-04-17 13:53:02 +0100

    updated hero content

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -2,27 +2,31 @@ const Hero = () => {
 	return (
 		<section className="home pt-5 pb-5" id="home">
 			<div className="row justify-content-evenly align-middle">
-				{/* here data-aos is used for adding aos animation  */}
 				<div
 					className="col-10 col-md-6 text-center text-md-start"
 					data-aos="slide-right"
 				>
 					<h2 style={{ color: "#3f396d" }}>
-						Hello! I Am <br />
+						Hello! I'm <br />
 						<span className="c-orange">Ayomide</span>
 					</h2>
 					<p className="text-muted">
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
-						omnis natus neque amet fuga error, delectus molestiae, nemo
-						provident deleniti quae laudantium veritatis rerum magni aliquam
-						reiciendis iste quo atque?
+						I'm a backend web developer with a passion for building efficient,
+						scalable, and secure server-side solutions. With a background in
+						physiotherapy, I bring a unique perspective to problem-solving and
+						user-centric design. My expertise includes working with databases
+						and APIs to create seamless and reliable web applications.
 					</p>
 					<button className="c-btn h-btn mt-3 py-3 px-5 rounded-pill">
 						Hire Me
 					</button>
 				</div>
-				<div className="col-10 col-md-4 d-none d-md-block">
-					<img src="/img/hero.png" className="img-fluid" alt="" />
+				<div className="col-10 col-md-4 mt-3 d-md-block">
+					<img
+						src="/img/hero.png"
+						className="img-fluid"
+						alt="Ayomide's Profile"
+					/>
 				</div>
 			</div>
 		</section>
